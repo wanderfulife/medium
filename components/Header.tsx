8bccc6cf1c07034c9ecfd204eb8b258059eff3bc
wanderfulife <john.wander@hotmail.fr>
2022-07-04 10:33:43 +1000

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Header() {
   return (
-	<div>
-	  <h1>I am a header</h1>
-	</div>
-  )
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className="w-44 object-contain cursor-pointer"
+            src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png"
+            alt=""
+          />
+        </Link>
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
+            Follow
+          </h3>
+        </div>
+      </div>
+	  <div className="flex items-center space-x-5 text-green-600">
+		<h3>Sign In</h3>
+		<h3 className="border px-4 py-1 rounded-full border-green-600">Get started</h3>
+	  </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,6 @@ const Home: NextPage = () => {
         <title>Medium blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>This is Medium 2.0</h1>
       <Header />
     </div>
   );
